e2cccf74c37bf11b74c59e235283304642a04560
Sofia Rozhko <soniarozhko@gmail.com>
2018-12-06 11:06:21 +0200

    [ADD] authorization settings

---
+++ WindowsFormsCiv/AuthorizationSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsCiv
+{
+    class AuthorizationSettings
+    {
+        Authorization authorization = new Authorization();
+
+        public string NickName { get; set; }
+        public string Civilization { get; set; }
+
+        public AuthorizationSettings()
+        {
+            NickName = authorization.tbNickname.Text;
+            Civilization = authorization.tbCivilzation.Text;
+
+        }
+}
+}
