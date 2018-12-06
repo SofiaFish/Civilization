ee3e510faaa7d8c68fde3f580e9113438f191c6f
Sofia Rozhko <soniarozhko@gmail.com>
2018-12-06 11:38:04 +0200

    [UPD] add treckBars settings and 3 resourses

--- WindowsFormsCiv/Form1.cs
+++ WindowsFormsCiv/Form1.cs
@@ -12,19 +12,48 @@ namespace WindowsFormsCiv
 {
     public partial class Civilization : Form
     {
+        Game game = new Game();
+        AuthorizationSettings settings = new AuthorizationSettings();
         public Civilization()
         {
             InitializeComponent();
+
+            //lCountry = settings.Civilization;
+            trackArmy.Minimum = game.MinCountArmy;
+            trackArmy.Maximum = game.MaxCountArmy;
+            trackTerritories.Minimum = game.MinCaptured;
+            trackTerritories.Maximum = game.MaxCaptured;
+            trackWarrios.Minimum = game.MinCountWarrios;
+            trackWarrios.Maximum = game.MaxCountWarrios;
+
+            
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void bBuy_Click(object sender, EventArgs e)
         {
-
+            game.Army += trackArmy.Value;
+            lArmy.Text = (trackArmy.Value).ToString();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void bSchool_Click(object sender, EventArgs e)
         {
+            game.School += 1;
+            if ((game.School) == 10)
+                game.Education += 5;
+        }
 
+        private void bChurch_Click(object sender, EventArgs e)
+        {
+            game.Church += 1;
+            if ((game.Church) == 5)
+                game.Religion += 10;
+        }
+
+        private void bBur_Click(object sender, EventArgs e)
+        {
+            game.Bar += 1;
+            if ((game.Bar) == 20)
+                game.HappinessLvl += 10;
         }
     }
 }
